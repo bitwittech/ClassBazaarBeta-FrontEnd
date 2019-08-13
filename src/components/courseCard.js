20a0c352ca974d2160b81a26b2a47c063410af2e
chakshu gautam <chakshu@samagragovernance.in>
2019-08-13 22:32:34 +0530

    Prettier; Card UI improvements

--- src/components/courseCard.js
+++ src/components/courseCard.js
@@ -3,44 +3,97 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import StarRatings from 'react-star-ratings';
+import Grid from '@material-ui/core/Grid';
+import WatchLaterIcon from '@material-ui/icons/WatchLater';
+import CalendarIcon from '@material-ui/icons/CalendarToday';
+import MoneyIcon from '@material-ui/icons/AttachMoney';
 
 const useStyles = makeStyles(theme => ({
   root: {
     padding: theme.spacing(3, 2),
   },
+  gridRoot: {
+    padding: theme.spacing(1, 1),
+  },
 }));
 
-const CourseCard = (props) => {
+const CourseCard = props => {
   const classes = useStyles();
+  const titleCase = str => {
+    return str
+      .replace(/[a-z]/i, function(letter) {
+        return letter.toUpperCase();
+      })
+      .trim();
+  };
 
   return (
-    <div>
-      <Paper className={classes.root}>
-        <Typography variant="h5" component="h3">
-          {props.university}
-        </Typography>
-        <Typography component="p">
-          {props.courseName}
-        </Typography>
-        <Typography component="p">
-          via {props.provider}
-        </Typography>
-        <div>
-            <StarRatings
-                rating={props.rating}
-                starRatedColor="#f1bd50"
-                numberOfStars={5}
-                name='rating'
-                starDimension="20px"
-                starSpacing="1px"
-            />
-            <Typography component="p">
-                {props.rating}
-            </Typography>
-        </div>
-      </Paper>
-    </div>
+    <Grid container className={classes.gridRoot} spacing={0}>
+      <Grid item xs={1}></Grid>
+      <Grid item xs={7}>
+        <Paper className={classes.root}>
+          <Grid container>
+            <Grid xs={7}>
+              <Typography variant="h5" component="h3">
+                {props.university}
+              </Typography>
+              <Typography component="p">{props.courseName}</Typography>
+              <Typography component="p">via {props.provider}</Typography>
+              <div>
+                <StarRatings
+                  rating={props.rating}
+                  starRatedColor="#f1bd50"
+                  numberOfStars={5}
+                  name="rating"
+                  starDimension="20px"
+                  starSpacing="1px"
+                />
+                <Typography component="p">{props.rating}</Typography>
+              </div>
+            </Grid>
+            <Grid xs={4} container spacing={2}>
+              <Grid xs={12}>
+                <div
+                  style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                  }}
+                >
+                  <WatchLaterIcon />
+                  <Typography spacing={1}>
+                    {titleCase(props.duration)}
+                  </Typography>
+                </div>
+              </Grid>
+              <Grid xs={12}>
+                <div
+                  style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                  }}
+                >
+                  <CalendarIcon />
+                  <Typography>{titleCase(props.startingOn)}</Typography>
+                </div>
+              </Grid>
+              <Grid xs={12}>
+                <div
+                  style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                  }}
+                >
+                  <MoneyIcon />
+                  <Typography>{titleCase(props.price)}</Typography>
+                </div>
+              </Grid>
+            </Grid>
+            <Grid xs={1}></Grid>
+          </Grid>
+        </Paper>
+      </Grid>
+    </Grid>
   );
-}
+};
 
 export default CourseCard;
--- src/components/homePage.js
+++ src/components/homePage.js
@@ -19,16 +19,51 @@ const styles = {
 
 class HomePage extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: []
+        };
+        this.getUniversityForUdemy = this.getUniversityForUdemy.bind(this);
+    }
+    
+
+    componentDidMount() {
+        var url = `http://35.224.4.2/api/courses/udemy`;
+        fetch(url).then(
+          response => response.json()
+        ).then(
+          json => {
+            console.log(json);
+            this.setState({data: json.data});
+          }
+        );
+      }
+
+    getUniversityForUdemy(obj){
+        let names = obj.visible_instructors.map(i => i.name + ", ").join("");
+        return names.slice(0, names.length-2);
+    }
+
     render(){
         return (
             <div>
-                <CourseCard 
-                    university={"Georgia Institute of Technology"} 
-                    courseName={"Machine Learning: Unsupervised Learning"} 
-                    provider={"Udacity"}
-                    
-                    rating={4.7}>
-                </CourseCard>
+                {this.state.data.length > 0 &&
+                 this.state.data.map((obj, index) => {
+                    return (<CourseCard
+                        key={obj.title}
+                        isInstructor={true}
+                        university={this.getUniversityForUdemy(obj)}
+                        courseName={obj.title} 
+                        provider={"Udacity"}
+                        duration={obj.content_info}
+                        startingOn={"Flexible"}
+                        price={obj.discount.price.price_string}
+                        rating={Math.round(obj.avg_rating*10)/10}>
+                    </CourseCard>)
+                 })
+                }
+                
             </div>
         );
     }
