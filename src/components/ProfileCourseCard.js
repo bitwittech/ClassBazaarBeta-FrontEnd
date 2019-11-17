5f1d373411cbf1fe557994302164b1bf98d32537
saranonearth <saran.suresh@outlook.com>
2019-11-17 19:04:35 +0530

    listing page redesign done

--- src/components/ProfileCourseCard.js
+++ src/components/ProfileCourseCard.js
@@ -6,6 +6,9 @@ import Paper from '@material-ui/core/Paper';
 import WatchLaterIcon from '@material-ui/icons/WatchLater';
 import MoneyIcon from '@material-ui/icons/AttachMoney';
 import CalendarIcon from '@material-ui/icons/CalendarToday';
+import TurnedInNotIcon from '@material-ui/icons/TurnedInNot';
+import TurnedInIcon from '@material-ui/icons/TurnedIn';
+
 const styles = {
   grid: {
     flexGrow: 1,
@@ -30,54 +33,86 @@ const styles = {
     backgroundColor: 'aliceblue',
   },
 };
-const ProfileCourseCard = () => {
+const ProfileCourseCard = props => {
   return (
     <>
-      <Paper style={{ padding: '15px' }}>
+      <div className="c-card">
+        <div className="coursecard-header">
+          <div>
+            <Typography
+              color="primary"
+              style={{ fontWeight: '600' }}
+              variant="subtitle1"
+              gutterBottom
+            >
+              {props.university}
+            </Typography>
+          </div>
+          <div>
+            <TurnedInNotIcon color="primary" />
+          </div>
+        </div>
         <Typography
-          color="primary"
-          style={{ fontWeight: '600' }}
-          variant="subtitle1"
+          variant="subtitle2"
+          style={{
+            color: '#3C3C3C',
+            fontWeight: '600',
+            padding: '0px 15px 0px 15px',
+          }}
           gutterBottom
         >
-          University Name
+          {props.courseName}
         </Typography>
         <Typography
-          variant="h5"
-          style={{ color: '#3C3C3C', fontWeight: '500' }}
+          style={{ padding: '0px 15px 0px 15px' }}
+          variant="caption"
+          display="block"
           gutterBottom
         >
-          Introduction to Computer Science : CS101
+          {props.provider}
         </Typography>
-        <Typography variant="caption" display="block" gutterBottom>
-          via edX
-        </Typography>
-        <Typography variant="body2" gutterBottom>
+        {/* <Typography
+          style={{ padding: '15px 15px 0px 15px' }}
+          variant="body2"
+          gutterBottom
+        >
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
           aliquam vitae ipsum sit amet egestas. Sed molestie dui id diam tempor,
           vel tristique turpis ullamcorper. In eget fringilla diam. Ut placerat
           justo eget tempor aliquam. Etiam bibendum in massa vehicula sagittis.
           Proin varius nisi mauris, id semper nulla rhoncus at.
-        </Typography>
+        </Typography> */}
         <br />
         <div>
-          <div className={styles.root}>
-            <Grid container spacing={2}>
+          <div
+            className={styles.root}
+            style={{
+              background: '#F4F2F2',
+              padding: '0px 15px 0px 15px',
+              borderRadius: '0px 0px 4px 4px',
+            }}
+          >
+            <Grid container align="left" spacing={2}>
               <Grid item sm={3}>
-                <WatchLaterIcon className="mb" color="primary" /> 1 month
+                <WatchLaterIcon
+                  fontSize="small"
+                  className="mb"
+                  color="primary"
+                />{' '}
+                1 month
               </Grid>
               <Grid item sm={3}>
-                <CalendarIcon className="mb" color="primary" /> Flexible
+                <CalendarIcon fontSize="small" className="mb" color="primary" />{' '}
+                Flexible
               </Grid>
               <Grid item sm={3}>
-                <MoneyIcon className="mb" color="primary" />
+                <MoneyIcon fontSize="small" className="mb" color="primary" />
                 Subscriptions
               </Grid>
             </Grid>
           </div>
         </div>
-      </Paper>
-      <br />
+      </div>
     </>
   );
 };
