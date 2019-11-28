ffd822625fb69685b6f9b9ddd90d8f18c9111da6
saranonearth <saran.suresh@outlook.com>
2019-11-28 21:55:56 +0530

    New Course Details Updated for Udemy|FL|SL|Edx

--- src/components/CourseDetails.jsx
+++ src/components/CourseDetails.jsx
@@ -10,6 +10,7 @@ import StarIcon from '@material-ui/icons/Star';
 import StarHalfIcon from '@material-ui/icons/StarHalf';
 import StarBorderIcon from '@material-ui/icons/StarBorder';
 import MonetizationOnIcon from '@material-ui/icons/MonetizationOn';
+import formatDate from './../utils/dateUtils';
 import ListAltIcon from '@material-ui/icons/ListAlt';
 import Box from '@material-ui/core/Box';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
@@ -21,7 +22,10 @@ const CourseDetails = props => {
     data: null,
   });
   const provider = props.match.params.provider;
-  const uuid = props.match.params.uuid;
+  let uuid = props.match.params.uuid;
+  if (provider === 'SimpliLearn') {
+    uuid = props.location.state.uuid.replace(/[`~!",.<>\{\}\[\]\\\/]/gi, '');
+  }
   useEffect(() => {
     const getCourseDetails = async () => {
       var url = `https://api.classbazaar.in/api/course?uuid=${uuid}&provider=${provider}`;
@@ -90,31 +94,44 @@ const CourseDetails = props => {
           <div>
             <QueryBuilderIcon color="primary" /> &nbsp;
           </div>
-          <div>8 hours per week, 4 weeks</div>
+          <div>{props.location.state.duration}</div>
         </div>
 
         <div style={{ display: 'flex', marginTop: '15px' }}>
           <div>
             <DateRangeIcon color="primary" /> &nbsp;
           </div>
-          <div>Starts on Sep.2</div>
+          <div>{` Starts on ${formatDate(
+            new Date(props.location.state.startingOn),
+            'MMMM d'
+          )}`}</div>
         </div>
 
         <div style={{ display: 'flex', marginTop: '15px' }}>
           <div>
             <MonetizationOnIcon color="primary" /> &nbsp;
           </div>
-          <div>Free</div>
+          <div>
+            {props.location.state.price === '' ||
+            props.location.state.price === null
+              ? 'Free'
+              : props.location.state.price}
+          </div>
         </div>
 
         <div style={{ display: 'flex', marginTop: '15px' }}>
           <div>
             <ListAltIcon color="primary" /> &nbsp;
           </div>
-          <div>edX</div>
+          <div>{provider}</div>
         </div>
         <div style={{ marginTop: '20px' }}>
-          <button className="enroll-btn">
+          <button
+            onClick={() => {
+              window.open(props.location.state.url, '_blank');
+            }}
+            className="enroll-btn"
+          >
             <div style={{ display: 'flex', flexDirection: 'row' }}>
               <div
                 style={{
@@ -302,10 +319,416 @@ const CourseDetails = props => {
       </Container>
     );
 
+  const udemy = () =>
+    state.data && (
+      <Container maxWidth="lg" style={{ marginTop: '40px' }}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={8}>
+            <div
+              style={{
+                background: '#fff',
+                padding: '20px',
+                paddingLeft: '40px',
+              }}
+            >
+              <div className="cd-head">
+                <div>
+                  <Typography
+                    style={{ fontWeight: '600' }}
+                    color="primary"
+                    variant="subtitle2"
+                    gutterBottom
+                  >
+                    {props.location.state.university}
+                  </Typography>
+                  <Typography variant="h6" gutterBottom>
+                    {state.data.title}
+                  </Typography>
+                  <Typography
+                    variant="caption"
+                    display="block"
+                    className="provider"
+                    gutterBottom
+                  >
+                    via {provider}
+                  </Typography>
+                </div>
+                <div style={{ textAlign: 'right' }}>{reviewSection()}</div>
+              </div>
+              <br />
+              <div className="cd-cont">
+                <Typography
+                  style={{ fontWeight: '600', fontSize: '22px' }}
+                  variant="subtitle2"
+                  gutterBottom
+                >
+                  Course Overview
+                </Typography>
+                <Typography
+                  style={{ fontSize: '16px', fontWeight: '300' }}
+                  variant="body1"
+                  gutterBottom
+                >
+                  {ReactHtmlParser(state.data.description)}
+                </Typography>
+                <br />
+                {state.data.outcome !== '' && (
+                  <>
+                    <Typography
+                      style={{ fontWeight: '600', fontSize: '22px' }}
+                      variant="subtitle2"
+                      gutterBottom
+                    >
+                      What will you learn?
+                    </Typography>
+                    <Typography
+                      style={{ fontSize: '16px', fontWeight: '300' }}
+                      variant="body1"
+                      gutterBottom
+                    >
+                      {state.data.what_you_will_learn_data.items.map((e, i) => (
+                        <li key={i}>{e}</li>
+                      ))}
+                    </Typography>{' '}
+                  </>
+                )}
+                {state.data.prerequisites_raw !== '' ? (
+                  <>
+                    {' '}
+                    <Typography
+                      style={{ fontWeight: '600', fontSize: '22px' }}
+                      variant="subtitle2"
+                      gutterBottom
+                    >
+                      Prerequisites
+                    </Typography>
+                    <Typography
+                      style={{ fontSize: '16px', fontWeight: '300' }}
+                      variant="body1"
+                      gutterBottom
+                    >
+                      {state.data.prerequisites.map((e, i) => (
+                        <li key={i}>{e}</li>
+                      ))}
+                    </Typography>
+                  </>
+                ) : null}
+                <br />
+
+                {state.data.closestRun !== undefined && (
+                  <>
+                    <Typography
+                      style={{ fontWeight: '600', fontSize: '22px' }}
+                      variant="subtitle2"
+                      gutterBottom
+                    >
+                      Professor:{' '}
+                      {this.state.closestRun.staff.map((obj, index) => (
+                        <span key={index} style={{ fontWeight: '300' }}>
+                          {obj.given_name}
+                        </span>
+                      ))}
+                    </Typography>
+                  </>
+                )}
+                <br />
+                <Typography
+                  style={{ fontWeight: '600', fontSize: '22px' }}
+                  variant="subtitle2"
+                  gutterBottom
+                >
+                  Reviews
+                </Typography>
+                <Grid
+                  container
+                  style={{ padding: 20, background: '#00000015' }}
+                >
+                  <Grid item xs={3}>
+                    <Grid item xs={12}>
+                      {/* <Fab color="primary" aria-label="add" className={classes.fab}>
+                  <AddIcon />
+                </Fab> */}
+                    </Grid>
+                  </Grid>
+                  <Grid item xs={9}>
+                    <Box style={{ padding: 30 }}>
+                      Natus error sit voluptartem accusantium doloremque
+                      laudantium, totam rem aperiam, eaque ipsa quae ab illo
+                      inventore.
+                    </Box>
+                  </Grid>
+                </Grid>
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            {courseSummary()}
+          </Grid>
+        </Grid>
+      </Container>
+    );
+
+  const fl = () =>
+    state.data && (
+      <Container maxWidth="lg" style={{ marginTop: '40px' }}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={8}>
+            <div
+              style={{
+                background: '#fff',
+                padding: '20px',
+                paddingLeft: '40px',
+              }}
+            >
+              <div className="cd-head">
+                <div>
+                  <Typography
+                    style={{ fontWeight: '600' }}
+                    color="primary"
+                    variant="subtitle2"
+                    gutterBottom
+                  >
+                    {state.data.organisation.name}
+                  </Typography>
+                  <Typography variant="h6" gutterBottom>
+                    {state.data.name}
+                  </Typography>
+                  <Typography
+                    variant="caption"
+                    display="block"
+                    className="provider"
+                    gutterBottom
+                  >
+                    via {provider}
+                  </Typography>
+                </div>
+                <div style={{ textAlign: 'right' }}>{reviewSection()}</div>
+              </div>
+              <br />
+              <div className="cd-cont">
+                <Typography
+                  style={{ fontWeight: '600', fontSize: '22px' }}
+                  variant="subtitle2"
+                  gutterBottom
+                >
+                  Course Overview
+                </Typography>
+                <Typography
+                  style={{ fontSize: '16px', fontWeight: '300' }}
+                  variant="body1"
+                  gutterBottom
+                >
+                  {ReactHtmlParser(state.data.description)}
+                </Typography>
+                <br />
+                {state.data.learning_outcomes !== '' && (
+                  <>
+                    <Typography
+                      style={{ fontWeight: '600', fontSize: '22px' }}
+                      variant="subtitle2"
+                      gutterBottom
+                    >
+                      What will you learn?
+                    </Typography>
+                    <Typography
+                      style={{ fontSize: '16px', fontWeight: '300' }}
+                      variant="body1"
+                      gutterBottom
+                    >
+                      {state.data.learning_outcomes.map((e, i) => (
+                        <li key={i}>{e}</li>
+                      ))}
+                    </Typography>{' '}
+                  </>
+                )}
+                {state.data.requirements !== '' && (
+                  <>
+                    <Typography
+                      style={{ fontWeight: '600', fontSize: '22px' }}
+                      variant="subtitle2"
+                      gutterBottom
+                    >
+                      Who is this course for?
+                    </Typography>
+                    <Typography
+                      style={{ fontSize: '16px', fontWeight: '300' }}
+                      variant="body1"
+                      gutterBottom
+                    >
+                      {state.data.requirements}
+                    </Typography>{' '}
+                  </>
+                )}
+
+                <br />
+
+                {state.data.educator !== '' && (
+                  <>
+                    <Typography
+                      style={{ fontWeight: '600', fontSize: '22px' }}
+                      variant="subtitle2"
+                      gutterBottom
+                    >
+                      Professor:{' '}
+                      <span style={{ fontWeight: '300' }}>
+                        {state.data.educator}
+                      </span>
+                    </Typography>
+                  </>
+                )}
+                <br />
+                <Typography
+                  style={{ fontWeight: '600', fontSize: '22px' }}
+                  variant="subtitle2"
+                  gutterBottom
+                >
+                  Reviews
+                </Typography>
+                <Grid
+                  container
+                  style={{ padding: 20, background: '#00000015' }}
+                >
+                  <Grid item xs={3}>
+                    <Grid item xs={12}>
+                      {/* <Fab color="primary" aria-label="add" className={classes.fab}>
+                  <AddIcon />
+                </Fab> */}
+                    </Grid>
+                  </Grid>
+                  <Grid item xs={9}>
+                    <Box style={{ padding: 30 }}>
+                      Natus error sit voluptartem accusantium doloremque
+                      laudantium, totam rem aperiam, eaque ipsa quae ab illo
+                      inventore.
+                    </Box>
+                  </Grid>
+                </Grid>
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            {courseSummary()}
+          </Grid>
+        </Grid>
+      </Container>
+    );
+
+  const sl = () =>
+    state.data && (
+      <Container maxWidth="lg" style={{ marginTop: '40px' }}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} sm={8}>
+            <div
+              style={{
+                background: '#fff',
+                padding: '20px',
+                paddingLeft: '40px',
+              }}
+            >
+              <div className="cd-head">
+                <div>
+                  <Typography
+                    style={{ fontWeight: '600' }}
+                    color="primary"
+                    variant="subtitle2"
+                    gutterBottom
+                  >
+                    {props.location.state.university}
+                  </Typography>
+                  <Typography variant="h6" gutterBottom>
+                    {ReactHtmlParser(state.data.courseData.fields.title)}
+                  </Typography>
+                  <Typography
+                    variant="caption"
+                    display="block"
+                    className="provider"
+                    gutterBottom
+                  >
+                    via {provider}
+                  </Typography>
+                </div>
+                <div style={{ textAlign: 'right' }}>{reviewSection()}</div>
+              </div>
+              <br />
+              <div className="cd-cont">
+                <Typography
+                  style={{ fontWeight: '600', fontSize: '22px' }}
+                  variant="subtitle2"
+                  gutterBottom
+                >
+                  Course Overview
+                </Typography>
+                <Typography
+                  style={{ fontSize: '16px', fontWeight: '300' }}
+                  variant="body1"
+                  gutterBottom
+                >
+                  {ReactHtmlParser(state.data.courseData.highlights.content)}
+                </Typography>
+
+                {state.data.closestRun !== undefined && (
+                  <>
+                    <Typography
+                      style={{ fontWeight: '600', fontSize: '22px' }}
+                      variant="subtitle2"
+                      gutterBottom
+                    >
+                      Professor:{' '}
+                      {this.state.closestRun.staff.map((obj, index) => (
+                        <span key={index} style={{ fontWeight: '300' }}>
+                          {obj.given_name}
+                        </span>
+                      ))}
+                    </Typography>
+                  </>
+                )}
+                <br />
+                <Typography
+                  style={{ fontWeight: '600', fontSize: '22px' }}
+                  variant="subtitle2"
+                  gutterBottom
+                >
+                  Reviews
+                </Typography>
+                <Grid
+                  container
+                  style={{ padding: 20, background: '#00000015' }}
+                >
+                  <Grid item xs={3}>
+                    <Grid item xs={12}>
+                      {/* <Fab color="primary" aria-label="add" className={classes.fab}>
+                <AddIcon />
+              </Fab> */}
+                    </Grid>
+                  </Grid>
+                  <Grid item xs={9}>
+                    <Box style={{ padding: 30 }}>
+                      Natus error sit voluptartem accusantium doloremque
+                      laudantium, totam rem aperiam, eaque ipsa quae ab illo
+                      inventore.
+                    </Box>
+                  </Grid>
+                </Grid>
+              </div>
+            </div>
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            {courseSummary()}
+          </Grid>
+        </Grid>
+      </Container>
+    );
+
   const renderSwitch = provider => {
     switch (provider) {
       case 'EDx':
         return edX();
+      case 'Udemy':
+        return udemy();
+      case 'FutureLearn':
+        return fl();
+      case 'SimpliLearn':
+        return sl();
       default:
         return <h1>Coming Soon</h1>;
     }
