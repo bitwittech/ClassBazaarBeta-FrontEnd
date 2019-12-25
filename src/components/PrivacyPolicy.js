82e4633e548c74485c5461ac6585ae40285874ce
saranonearth <saran.suresh@outlook.com>
2019-12-25 19:49:35 +0530

    Provacy Policy content

--- src/components/PrivacyPolicy.js
+++ src/components/PrivacyPolicy.js
@@ -3,12 +3,13 @@ import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import Footer from './Footer';
 
 const ProvacyPolicy = () => {
   return (
     <>
       <AppBar />
-      <Container maxWidth="lg">
+      <Container maxWidth="lg" style={{ background: '#fff !important' }}>
         <br />
         <Typography
           variant="subtitle1"
@@ -28,34 +29,45 @@ const ProvacyPolicy = () => {
             padding: '15px',
           }}
         >
+          <Typography
+            variant="h6"
+            color="primary"
+            style={{ fontWeight: '500' }}
+          >
+            Your privacy is valuable to us!
+          </Typography>
           <br />
-          <strong>Your privacy is valuable to us!</strong> <br />
           The purpose of this Privacy Policy is to describe how Class Bazaar,
           LLP. (“Class Bazaar,” “us,” “we,” or “our”) collects, uses and shares
-          data about you through our website http://www.classbazaar.com
-          (referred to herein as the “Site”, ”website”). This Privacy Policy
-          applies only to your use of our website. Note that provider websites
-          who provide the online courses and content found on our Site may also
-          gather information and you should consult those other parties’ privacy
-          policies as appropriate and applicable. These external sites are not
-          operated by us. Please be aware that we have no control over the
-          content and practices of these sites, and cannot accept responsibility
-          or liability for their respective privacy policies or content. Class
-          Bazaar does not process any payments on its Site, all transactions
-          happen between you (the user) and the content provider website. In
-          case of any issue please reach out to the content provider directly.
+          data about you through our website{' '}
+          <a href="http://www.classbazaar.com" style={{ color: '#FFA502' }}>
+            http://www.classbazaar.com
+          </a>
+          (referred to herein as the “Site”, ”website”).
+          <br />
+          <br />
+          This Privacy Policy applies only to your use of our website. Note that
+          provider websites who provide the online courses and content found on
+          our Site may also gather information and you should consult those
+          other parties’ privacy policies as appropriate and applicable. These
+          external sites are not operated by us. Please be aware that we have no
+          control over the content and practices of these sites, and cannot
+          accept responsibility or liability for their respective privacy
+          policies or content. Class Bazaar does not process any payments on its
+          Site, all transactions happen between you (the user) and the content
+          provider website. In case of any issue please reach out to the content
+          provider directly.
           <br />
           <br />
           <strong>Data Collection </strong>
-          <br />
           <br />
           We only ask for personal data when we truly need it to provide a
           service to you. We collect it by fair and lawful means, with your
           knowledge and consent. We also let you know why we’re collecting it
           and how it will be used in this Privacy Policy.
           <br />
-          The key terms used in this policy can be defined as :<br />
           <br />
+          The key terms used in this policy can be defined as :<br />
           <strong> 1. Personally Identifiable Information (“PII”) -</strong> Any
           data which can identify a person, including but not limited to name,
           email, profile photo. If you reside or are located in the European
@@ -84,8 +96,20 @@ const ProvacyPolicy = () => {
           unsubscribe link or instructions provided in any email that we send.
           <br />
           <br />
-          <strong>Usage Data </strong>
+          <u> Personally Identifiable Information (“PII”)</u>
           <br />
+          We collect PII to improve our service for you and provide with
+          personalised recommendations on courses. Sometimes we may receive PII
+          when you access or log-in to a third party site, e.g., Facebook or
+          Google, from our Site. This may include the text and/or images of your
+          Personally Identifiable Information available from the third party
+          site. We may use your PII data like email to communicate with you
+          about important updates. You may opt out of receiving these
+          communications from us by following the unsubscribe link or
+          instructions provided in any email that we send.
+          <br />
+          <br />
+          <u> Usage Data </u>
           <br />
           When users come to our Site, we may track and collect data on how is
           the Site used or accessed i.e. “Usage Data” - including but not
@@ -97,18 +121,22 @@ const ProvacyPolicy = () => {
           course have you spent the most time exploring. We may also use web
           beacons and cookies to tailor your user experience.
           <br />
-          Public Data The content generated by user including but not limited to
-          reviews and bookmarks for is intended for public consumption and will
-          be displayed through our Site.
+          <br />
+          <u>Public Data </u>
+          <br />
+          The content generated by user including but not limited to reviews and
+          bookmarks for is intended for public consumption and will be displayed
+          through our Site. <br />
           <br />
           We do not honour any “Do Not Track” signals which you can configure in
           the settings of your browser.
           <br />
           <br />
-          Data Usage <br />
+          <strong>Data Usage </strong>
           <br />
           We will NEVER sell your Personally Identifiable Information. — pinky
           promise!
+          <br />
           <br />
           Your use of our Site will be regarded as acceptance of our practices
           around privacy and personal information. If you have any questions
@@ -124,19 +152,18 @@ const ProvacyPolicy = () => {
           reach out to you; 9. to provide any technical or customer support if
           needed while using our Site; 10. to monitor the usage of our Site.
           <br />
+          <br />
           If you are from the European Economic Area (EEA), Class Bazaar, legal
           basis for collecting and using PII and other data described in this
           Privacy Policy depends on what data we collect and the specific
           context in which we collect it. We may collect your PII because : 1.
           We need to execute a contract with you 2. You have allowed us to
           communicate with you 3. We got to comply with the law
-          <br />
-          <strong>Data Storage & Security </strong>
-          <br />
-          <br />
+          <strong>Data Storage & Security </strong> <br />
           We only retain the collected data for as long as necessary to provide
           you with a personalised service and as required to comply with
           applicable laws.
+          <br />
           <br />
           We strive to keep your data secure and store all of our data on Amazon
           Web Service and Google Cloud Business servers. Please be informed that
@@ -146,8 +173,10 @@ const ProvacyPolicy = () => {
           However, as you know nothing can guarantee 100% security of the data
           over the Internet.
           <br />
+          <br />
           Disclosure of Data We will never share your PII data publicly or with
           third-parties, except when :
+          <br />
           <br />
           1. Merger, Acquistion or Asset Sale — if Class Bazaar is involved in a
           merger, acquisition or asset sale, your data may be transferred. We
@@ -159,11 +188,13 @@ const ProvacyPolicy = () => {
           with our service and a customer service representative reaches out to
           you.
           <br />
+          <br />
           International Policy Rights for EEA Residents under GDPR If you are a
           resident of European Economic Area (EEA) then you have certain rights
           with regards to your data. If you wish you exercise any of these
-          rights please contact us at info@classbazaar.com. We may ask you to
+          rights please contact us at ____________________. We may ask you to
           verify your identify before we comply with any such requests.
+          <br />
           <br />
           Rights : 1. Rights of access — You have the right to ask us to access,
           update or delete your PII. 2. Right to data portability — You have the
@@ -175,6 +206,7 @@ const ProvacyPolicy = () => {
           to object — You have the right to object or restrict us processing
           your PII.
           <br />
+          <br />
           Children’s Privacy Class Bazaar strongly believes in protecting the
           privacy of “Children” (below 13 years). Our Site is not intended for
           Children. Class Bazaar does not knowingly collect any Personal
@@ -183,28 +215,33 @@ const ProvacyPolicy = () => {
           you to contact us immediately and we will promptly remove such
           information from our servers.
           <br />
+          <br />
           California Under California’s "Shine the Light" law, California
           residents who provide personal information in obtaining products or
           services for personal, family or household use are entitled to request
           and obtain from us once a calendar year information about the customer
           information we shared, if any, with other businesses for their own
           direct marketing uses. If you wish to obtain this data please contact
-          us at info@classbazaar.com. Note that not all data is covered by this
+          us at ____________________. Note that not all data is covered by this
           law and only data covered by this law will be shared with you.
+          <br />
           <br />
           Legal Proceedings Class Bazaar is a registered corporation in Mumbai,
           India and all the legal proceedings must be done under the
           jurisdiction of the local courts of the district of Mumbai.
           <br />
+          <br />
           Updates to Privacy Policy We review our privacy practices from time to
           time to maintain the security of your data. Any update will be
           effective immediately once we post it to our Site. We will notify you
           about the changes within a reasonable period of time. <br />
+          <br />
         </div>
         <br />
         <br />
         <br />
       </Container>
+      <Footer />
     </>
   );
 };
