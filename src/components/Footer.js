b8ce46de028129ebc4b00577bef4459ce4bc96d0
saranonearth <saran.suresh@outlook.com>
2019-12-28 17:46:05 +0530

    Mobile UI Footer: Home Modal: Landing Page Cards

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,11 @@ const Footer = ({ bgColor }) => {
         <div style={{ marginTop: '20px' }}>
           <img className="footer-logo" src={Logo} alt="classbazarLogo" />
         </div>
-        <div className="footer-links">
+        <div className="footer-text-m no-desktop">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur
+          ipsum dolor si conseq int sapiente obcaecati animi
+        </div>
+        <div className="footer-links no-mobile">
           <div>
             <p>
               <a href="/about">About Us</a>
@@ -32,9 +36,13 @@ const Footer = ({ bgColor }) => {
             </p>
           </div>
         </div>
-        <p class="footer-text"> Email: info@classbazaar.com </p>
-        <p class="footer-text tsm">
-          © Copyright 2019 <strong>Class Bazaar</strong>, All Right Reserved
+        <p className="footer-text mt-2">Phone: +1 234 567 9332</p>
+        <p className="footer-text">
+          Email: info@company.com | Fax: +1 342 422 5314
+        </p>
+        <p className="footer-text tsm">
+          © Copyright 2019 <span className="bold">Class Bazaar</span>, All Right
+          Reserved
         </p>
       </div>
     </>
