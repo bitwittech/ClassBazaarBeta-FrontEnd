8c4c7d747d3580cf47c13eb2a1a3362b4c2ee961
saranonearth <saran.suresh@outlook.com>
2020-01-21 21:54:34 +0530

    Profile Review Placeholders Chnaged

--- src/components/profilePage.js
+++ src/components/profilePage.js
@@ -32,7 +32,11 @@ import AlertSnackbar from './AlertSnackbar';
 import { titleCase } from './../utils/utils';
 import { Redirect, withRouter } from 'react-router';
 import SaveIcon from '@material-ui/icons/Save';
-import { updateUser, updatePassword, updateLocation } from '../actions/ContextActions';
+import {
+  updateUser,
+  updatePassword,
+  updateLocation,
+} from '../actions/ContextActions';
 import { Link } from 'react-router-dom';
 
 const styles = {
@@ -81,7 +85,7 @@ class ProfilePage extends Component {
       curPassword: '',
       msg: null,
       rLoading: true,
-      userImage:'',
+      userImage: '',
       reviews: [],
     };
     this.updateData = this.updateData.bind(this);
@@ -95,9 +99,10 @@ class ProfilePage extends Component {
         user,
         phone: user.mobilePhone,
         email: user.email,
-        userImage: user.data && user.data.image
-        ? user.data && user.data.image
-        : 'https://www.sketchengine.eu/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png',
+        userImage:
+          user.data && user.data.image
+            ? user.data && user.data.image
+            : 'https://www.sketchengine.eu/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png',
         location:
           user.data && user.data.location
             ? user.data && user.data.location
@@ -107,8 +112,8 @@ class ProfilePage extends Component {
       if (user.data === undefined) {
         return this.setState({ data: [], reviews: [], loading: false });
       }
-      if(user.data.bookmarks.length === 0){
-        return this.setState({data:[],loading:false})
+      if (user.data.bookmarks.length === 0) {
+        return this.setState({ data: [], loading: false });
       }
 
       const data = user.data.bookmarks;
@@ -164,7 +169,7 @@ class ProfilePage extends Component {
 
   render() {
     if (this.state.user == null) return <Redirect to="/" />;
-    console.log("STATE PROFILE",this.state)
+    console.log('STATE PROFILE', this.state);
     return (
       <>
         <TopAppBar />
@@ -269,19 +274,26 @@ class ProfilePage extends Component {
                                 ) : (
                                   <SaveIcon
                                     onClick={async () => {
-                                     try{
-                                      const res = await updateLocation(
-                                        this.state.user.id,
-                                        this.state.location,
-                                        this.state.user
-                                      );
-                                      this.setState({ locationB: false,msg:res });
-                                     }catch{
-                                      this.setState({ locationB: false,msg:{
-                                        varient: 'error',
-                                        message:"Unable to change location"
-                                      } });
-                                     }
+                                      try {
+                                        const res = await updateLocation(
+                                          this.state.user.id,
+                                          this.state.location,
+                                          this.state.user
+                                        );
+                                        this.setState({
+                                          locationB: false,
+                                          msg: res,
+                                        });
+                                      } catch {
+                                        this.setState({
+                                          locationB: false,
+                                          msg: {
+                                            varient: 'error',
+                                            message:
+                                              'Unable to change location',
+                                          },
+                                        });
+                                      }
                                     }}
                                     color="primary"
                                     style={{ fontSize: '1.2em' }}
@@ -507,20 +519,23 @@ class ProfilePage extends Component {
                                 ) : (
                                   <SaveIcon
                                     onClick={async () => {
-                                      try{
+                                      try {
                                         const {
-                                        curPassword,
-                                        newPassword,
-                                      } = this.state;
+                                          curPassword,
+                                          newPassword,
+                                        } = this.state;
 
-                                      const res = await updatePassword(
-                                        curPassword,
-                                        newPassword,
-                                        this.state.user.email,
-                                        this.state.user.id
-                                      );
-                                      this.setState({ changePasswordB: false,msg: res });
-                                      }catch(error){
+                                        const res = await updatePassword(
+                                          curPassword,
+                                          newPassword,
+                                          this.state.user.email,
+                                          this.state.user.id
+                                        );
+                                        this.setState({
+                                          changePasswordB: false,
+                                          msg: res,
+                                        });
+                                      } catch (error) {
                                         this.setState({
                                           changePasswordB: false,
                                           msg: {
@@ -611,7 +626,7 @@ class ProfilePage extends Component {
                                   className="hover"
                                   gutterBottom
                                 >
-                                  university
+                                  {data.course && data.course.university}
                                 </Typography>
                               </div>
                               <div></div>
@@ -629,7 +644,7 @@ class ProfilePage extends Component {
                                 className="hover"
                                 gutterBottom
                               >
-                                Course Name
+                                {data.course && data.course.title}
                               </Typography>
                             </Link>
                             <Typography
@@ -641,7 +656,7 @@ class ProfilePage extends Component {
                               display="block"
                               gutterBottom
                             >
-                              {data.provider}
+                              {data.review.provider}
                             </Typography>
                             <div>
                               <Typography
@@ -666,7 +681,7 @@ class ProfilePage extends Component {
                                 display="block"
                                 gutterBottom
                               >
-                                {data.review}
+                                {data.review.review}
                               </Typography>
                             </div>
                             <br />
