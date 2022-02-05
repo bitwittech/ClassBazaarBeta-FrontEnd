9ab8a92bc9437a6c63da4e8a8f186f273356942a
ClassBazaar <61837338+ClassBazaar@users.noreply.github.com>
2022-02-05 11:46:46 +0530

    Added Converter.js for price conversion

---
+++ src/components/converter.js
@@ -0,0 +1,31 @@
+import React,{useEffect,useState} from 'react'
+import axios from "axios";
+
+
+function Converter(props)
+{
+    const [price,setPrice] = useState()
+        
+    const data = () => {
+  
+        
+        let  url = `https://freecurrencyapi.net/api/v2/latest?apikey=45f68830-84f3-11ec-8258-811245eebca2&base_currency=${props.currency}`;
+        axios.get(url).then((response)=>{
+
+            let p = props.price * response.data.data.INR;
+            p = Math.round(p)
+            setPrice(p)
+        })
+};
+
+
+    return(
+        <>
+        {data()}
+        {price}
+        </>
+    )
+
+}
+
+export default Converter;
