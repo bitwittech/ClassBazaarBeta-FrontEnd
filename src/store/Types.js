aeffecf5bc2f62be6afcdb9be7d09b7c59fc51c4
ClassBazaar <61837338+ClassBazaar@users.noreply.github.com>
2022-02-16 18:56:50 +0530

    updated for prelog

--- src/store/Types.js
+++ src/store/Types.js
@@ -1,4 +1,5 @@
 export const LOGIN_MODAL = 'LOGIN_MODAL';
+export const Pre_LOG_Box = 'Pre_LOG_Box';
 export const LOADING = 'LOADING';
 export const ALERT = 'ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
